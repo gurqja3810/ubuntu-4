8d80d0155ae25637ccfee16051433d32955c1f7c
KwonHyeokBeom <gurqja3810@naver.com>
2022-09-28 19:29:12 +0900

    discrimination odd and even

---
+++ c/eod.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int eod(int x){
+  if(x%2==0){
+    printf("%d is even number~!!\n",x);
+  }
+  else
+    printf("%d is odd number~!!\n",x);
+}
---
+++ c/report1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "libeod.h"
+
+void main() {
+  int x;
+  printf("Input number : ");
+  scanf("%d", &x);
+  eod(x);
+}
